6f00757ca419eb0fd68885486af7d8b43cc68158
rbrown29 <53094729+rbrown29@users.noreply.github.com>
2024-06-24 11:06:12 -0700

    update

--- angles-rest-map/src/Map.js
+++ angles-rest-map/src/Map.js
@@ -5,8 +5,8 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 const Map = () => {
    const isMobile = window.innerWidth < 768;
     const [viewport, setViewport] = useState({
-        latitude: 42.908,
-        longitude:  -122.132,
+        latitude: 45.049,
+        longitude:  -123.998,
         zoom: isMobile ? 13.68 : 14.68,
         bearing: isMobile ? 120 : 100,
         pitch: isMobile ? 50 : 50,
@@ -15,7 +15,7 @@ const Map = () => {
     const [geojson, setGeojson] = useState(null);
 
     useEffect(() => {
-        fetch('/GarfieldPeakHike.geojson')
+        fetch('/CascadeHeadHike.geojson')
             .then((response) => {
                 if (!response.ok) {
                     throw new Error('Network response was not ok ' + response.statusText);
