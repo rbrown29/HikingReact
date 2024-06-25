0d16f2775d2ea9b57c8be440640c3fe289973a63
rbrown29 <53094729+rbrown29@users.noreply.github.com>
2024-06-25 12:15:28 -0700

    update

--- angles-rest-map/src/Map.js
+++ angles-rest-map/src/Map.js
@@ -5,9 +5,9 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 const Map = () => {
    const isMobile = window.innerWidth < 768;
     const [viewport, setViewport] = useState({
-        latitude: 45.217,
-        longitude:  -123.973,
-        zoom: isMobile ? 13.68 : 15.68,
+        latitude: 44.3118,
+        longitude:  -122.02616,
+        zoom: isMobile ? 12.68 : 14.20,
         bearing: isMobile ? 120 : 100,
         pitch: isMobile ? 50 : 50,
     });
@@ -15,7 +15,7 @@ const Map = () => {
     const [geojson, setGeojson] = useState(null);
 
     useEffect(() => {
-        fetch('/CapeKiwanda.geojson')
+        fetch('/TamolitchPoolHike.geojson')
             .then((response) => {
                 if (!response.ok) {
                     throw new Error('Network response was not ok ' + response.statusText);
@@ -25,7 +25,7 @@ const Map = () => {
             .then((data) => setGeojson(data))
             .catch((error) => console.error('Error fetching the GeoJSON file:', error));
     }, []);
-
+    console.log(geojson);
     const mapStyle = 'mapbox://styles/digit9/clxi9m3kh005u01pugx4x23n6';
 
     return (
